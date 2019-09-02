d75371a250f40e4df238c5a509a976c196a981e7
Amelekesov <aleksey.melekesov@rt.ru>
2019-09-02 17:04:36 +0400

    Added base markup.

--- src/components/SortProducts.js
+++ src/components/SortProducts.js
@@ -4,10 +4,14 @@ import {
   Text,
   View,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import ActionSheet from 'react-native-actionsheet';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import Button from './Button';
 import i18n from '../utils/i18n';
+import Icon from './Icon';
 
 const styles = EStyleSheet.create({
   wrapper: {
@@ -18,6 +22,9 @@ const styles = EStyleSheet.create({
     paddingLeft: 14,
     paddingRight: 14,
     marginBottom: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   btn: {
     justifyContent: 'center',
@@ -26,9 +33,84 @@ const styles = EStyleSheet.create({
     paddingTop: 6,
     paddingBottom: 6,
   },
+  btnFilter: {
+    justifyContent: 'center',
+    paddingLeft: 6,
+    paddingRight: 6,
+    paddingTop: 6,
+    paddingBottom: 6,
+    marginRight: 20,
+  },
   text: {
     color: '$primaryColor',
     fontSize: '0.9rem',
+  },
+  filterText: {
+    color: '#8e8e8e',
+    fontSize: '0.9rem',
+  },
+  filterIcon: {
+    fontSize: '1.2rem',
+    color: '#8e8e8e',
+    position: 'absolute',
+    top: 4,
+    left: -20,
+  },
+  badge: {
+    backgroundColor: '#0093ff',
+    minWidth: 20,
+    height: 20,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    right: -16,
+  },
+  badgeText: {
+    color: '#fff',
+  },
+  filterHeaderSection: {
+    borderBottomWidth: 0.5,
+    borderColor: '#f1f1f1',
+    padding: 10,
+    paddingTop: 16,
+    paddingBottom: 14,
+    flexDirection: 'row',
+    width: '100%'
+  },
+  scrollWrapper: {
+    paddingTop: 4,
+    marginLeft: 20,
+  },
+  filterFooterSection: {
+    borderTopWidth: 0.5,
+    borderColor: '#f1f1f1',
+    padding: 40,
+    paddingTop: 16,
+    paddingBottom: 14,
+  },
+  scrollWrapperContent: {
+    minHeight: 310,
+  },
+  pickerWrapper: {
+    padding: 20,
+    borderTopWidth: 0.5,
+    borderColor: '#f1f1f1',
+  },
+  pickerOpenBtn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  pickerContent: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    marginTop: 10,
+  },
+  pickerOpenBtnText: {
+    fontSize: 20,
+    color: '#000'
   }
 });
 
@@ -96,6 +178,10 @@ class SortProducts extends Component {
     })
   };
 
+  state = {
+    openIDs: [],
+  };
+
   showActionSheet = () => {
     this.ActionSheet.show();
   }
@@ -111,6 +197,60 @@ class SortProducts extends Component {
 
     onChange(itemsList[foundIndex].params, foundIndex);
   };
+
+  renderPicker = () => (
+    <View style={styles.pickerWrapper}>
+      <TouchableOpacity style={styles.pickerOpenBtn}>
+        <Text style={styles.pickerOpenBtnText}>Size</Text>
+        <Icon name="arrow-drop-down" />
+      </TouchableOpacity>
+      <View style={styles.pickerContent}>
+        <Button type="label">
+          Red
+        </Button>
+        <Button type="labelActive">
+          Red
+        </Button>
+        <Button type="label">
+          Red
+        </Button>
+      </View>
+    </View>
+  );
+
+  renderHader = () => (
+    <View style={styles.filterHeaderSection}>
+      <TouchableOpacity onPress={() => this.RBSheet.close()}>
+        <Icon name="close" />
+      </TouchableOpacity>
+      <ScrollView horizontal contentContainerStyle={styles.scrollWrapper}>
+        <Button type="ghost">
+          {i18n.gettext('Clear all')}
+        </Button>
+        <Button type="round" clear>
+          Size
+        </Button>
+      </ScrollView>
+    </View>
+  );
+
+  renderFooter = () => (
+    <View style={styles.filterFooterSection}>
+      <Button type="primary">
+        {i18n.gettext('Apply')}
+      </Button>
+    </View>
+  );
+
+  renderFilters = () => (
+    <View>
+      {this.renderHader()}
+      <ScrollView contentContainerStyle={styles.scrollWrapperContent}>
+        {this.renderPicker()}
+      </ScrollView>
+      {this.renderFooter()}
+    </View>
+  );
 
   render() {
     const { sortParams } = this.props;
@@ -133,6 +273,22 @@ class SortProducts extends Component {
             {items[activeIndex]}
           </Text>
         </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.btnFilter}
+          onPress={() => {
+            this.RBSheet.open();
+          }}
+        >
+          <Icon name="filter-list" style={styles.filterIcon} />
+          <Text style={styles.text} numberOfLines={2}>
+            {i18n.gettext('Filter')}
+          </Text>
+          <View style={styles.badge}>
+            <Text style={styles.badgeText}>2</Text>
+          </View>
+        </TouchableOpacity>
+
         <ActionSheet
           ref={(ref) => { this.ActionSheet = ref; }}
           options={filteredItems}
@@ -140,6 +296,14 @@ class SortProducts extends Component {
           destructiveButtonIndex={CANCEL_INDEX}
           onPress={index => this.handleChange(filteredItems[index])}
         />
+        <RBSheet
+          ref={ref => { this.RBSheet = ref; }}
+          closeOnDragDown={false}
+          height={450}
+          duration={150}
+        >
+          {this.renderFilters()}
+        </RBSheet>
       </View>
     );
   }
