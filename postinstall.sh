32d7aa5e86bc3d8537c882378aa870aba04ba77e
Amelekesov <aleksey.melekesov@rt.ru>
2020-05-06 13:31:19 +0400

    Fix postinstall script.

--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Remove old react webview"
-if [ -e ./node_modules/react-native/React/RCTWebView.h ] ; then
-  rm -rf ./node_modules/react-native/React/RCTWebView.h
-  rm -rf ./node_modules/react-native/React/RCTWebView.m
-  rm -rf ./node_modules/react-native/React/RCTWebViewManager.h
-  rm -rf ./node_modules/react-native/React/RCTWebViewManager.m
+if [ -e ./node_modules/react-native/React/Views/RCTWebView.h ] ; then
+  rm -rf ./node_modules/react-native/React/Views/RCTWebView.h
+  rm -rf ./node_modules/react-native/React/Views/RCTWebView.m
+  rm -rf ./node_modules/react-native/React/Views/RCTWebViewManager.h
+  rm -rf ./node_modules/react-native/React/Views/RCTWebViewManager.m
 fi
